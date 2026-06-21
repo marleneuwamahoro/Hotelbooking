fa37a29e4ce8ab1539799ecc3d4e0f638d080262
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-06-21 14:22:12 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3A57E8',
+        info: '#08B1BA',
+        pagebg: '#F5F6FA',
+      },
+      fontFamily: {
+        sans: ['Inter', 'Poppins', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
